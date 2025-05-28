1535d2f61b9d20938dce2a26439ad6361e74f3ac
tarunsharma-tech <ts130043@gmail.com>
2025-05-28 13:04:32 +0000

    conversion of data types

---
+++ .github/01_basic/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let age = "20"
+
+console.log(typeof age)
+
+let ValueInNumber = Number(age)
+
+console.log(typeof ValueInNumber)
+
+// "20" => 20
+// "20abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Tarun"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Tarun" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
