ffaaf64ae2b26d04a76cc7858be6b56306ac756a
tarunsharma-tech <ts130043@gmail.com>
2025-05-29 16:09:51 +0000

    datatype_summery

---
+++ .github/01_basic/datatype_summery.js
@@ -0,0 +1,36 @@
+// Perimetive
+// 7 types : String, Number, BigInt, Boolean, undefined, null, Symbol
+
+
+const age = 20
+const agevalue = 78
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;  
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "TARUN",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
